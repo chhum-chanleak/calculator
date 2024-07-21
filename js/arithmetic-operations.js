0331ca4fdb13a4e80513d9189f1709f319a76a68
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-07-21 20:07:51 +0700

    feat: Implement operations(), which takes in three arguments 'operatorType, num1, num2' and returns a value depends on 'operatorType'

--- js/arithmetic-operations.js
+++ js/arithmetic-operations.js
@@ -1,9 +1,16 @@
-// Addition
-const add = (...numbers) => {
-  let result = 0;
-
-  for (let i = 0; i < numbers.length; i += 1) {
-    result += numbers[i];
-  }
-  return result;
+// Operate basic arithmetics operation and return result
+const operations = (operatorType, num1, num2) => {
+  
+  switch (operatorType) {
+    case '+':
+      return parseInt(num1) + parseInt(num2);
+    case '-':
+      return parseInt(num1) - parseInt(num2);
+    case '*':
+      return parseInt(num1) * parseInt(num2);
+    case '/':
+      return parseInt(num1) / parseInt(num2);
+    default:
+      return 'Unknown operator';
+  };
 };
