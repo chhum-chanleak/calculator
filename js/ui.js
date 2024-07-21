07bb7832ce179f378515863a74127c1e63cafd48
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-07-21 09:14:15 +0700

    refactor: add class 'button' to numerical buttons

--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,4 @@
+// Create buttons for numbers
 const createNumericalButtons = () => {
   const numericalButtons = document.querySelector('.numerical-buttons');
   
@@ -5,9 +6,44 @@ const createNumericalButtons = () => {
     const button = document.createElement('button');    
 
     button.textContent = `${i}`;
-    button.setAttribute('class', `numerical-button number-${i}`);
+    button.setAttribute('class', `numerical-button number-${i} button`);
     numericalButtons.appendChild(button);    
   }
-
 };
+
+// Create buttons for basic operator (* / + -)
+const createOperatorButtons = () => {
+  const operators = document.querySelector('.operators');
+
+  for (let i = 0; i < 5; i += 1) {
+    const button = document.createElement('button');
+
+    switch (i) {
+      case 0:
+        button.textContent = '*';
+        button.setAttribute('class', 'operator-button multiplication button');
+        break;
+      case 1:
+        button.textContent = '/';
+        button.setAttribute('class', 'operator-button division button');
+        break;
+      case 2:
+        button.textContent = '+';
+        button.setAttribute('class', 'operator-button add button');
+        break;
+      case 3:
+        button.textContent = '-';
+        button.setAttribute('class', 'operator-button subtraction button');
+        break;
+      case 4:
+        button.textContent = '=';
+        button.setAttribute('class', 'operator-button subtraction button');
+        break;
+    }
+    operators.appendChild(button);    
+  }  
+};
+
+
 createNumericalButtons();
+createOperatorButtons();
